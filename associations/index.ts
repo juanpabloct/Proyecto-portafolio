609d2fbe9e0c3773b31a62dac6e962e62afae303
juanpabloct <juanptorres2003@gmail.com>
2023-01-21 00:41:55 -0500

    Archivos borrados

--- associations/index.ts
+++
@@ -1,5 +0,0 @@
-import informationUser from "../models/informacionUser";
-import modelUser from "../models/user";
-
-const allInformation = modelUser.belongsTo(informationUser);
-export default allInformation;
--- models/usuarios.js
+++
@@ -1,5 +0,0 @@
-const { Model } = require("sequelize");
-
-const User = Model.define("users", {
-
-})
