f68e432c70032705fc04225d5c94a69817f69361
juanpabloct <juanptorres2003@gmail.com>
2023-02-01 22:32:36 -0500

    Endpoints del user

--- router/user.ts
+++ router/user.ts
@@ -1,7 +1,35 @@
-import { Request, Response, Router } from "express";
-import { CreateUser } from "../controllers/user";
-
+import { Prisma, User } from "@prisma/client";
+import { Request, Response, Router, RequestHandler } from "express";
+import { CreateUser } from "../controllers/user/CreateUser";
+import { LoginUser } from "../controllers/user/loginUser";
+import { ValidUserNotExist } from "../midlewares/validUserNotExist";
+interface ReqParams {
+  email: string;
+  password: string;
+}
+interface ErrorUser {
+  error: string;
+  status: number;
+}
 const app = Router();
-app.get("/newUser", (req: Request, res: Response) => {
-  CreateUser(req.body);
+app.post("/newUser", ValidUserNotExist, async (req: Request, res: Response) => {
+  const newUser = CreateUser(req.body)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err: ErrorUser) => {
+      res.status(err.status).json(err.error);
+    });
 });
+app.get("/login", async (req: Request, res) => {
+  const infoLogin = req.query as Prisma.UserEmailPasswordCompoundUniqueInput;
+  const loginUser = LoginUser(infoLogin)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err: ErrorUser) => {
+      res.status(err.status).json(err.error);
+    });
+});
+
+module.exports = app;
