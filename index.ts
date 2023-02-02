e965c3ac97b1e1aba9117c9c36f069fb7e5dd98f
juanpabloct <juanptorres2003@gmail.com>
2023-02-01 22:27:36 -0500

    Se cambio el puerto de escucha

--- index.ts
+++ index.ts
@@ -2,12 +2,11 @@ import { client } from "./client";
 
 const express = require("express");
 const app = express();
-
 app.use(express.json());
 app.use("/user", require("./router/user"));
 
 client.$connect().then(() => {
-  app.listen(3000, () => {
-    console.log(`Run in Port ${3000}`);
+  app.listen(9000, () => {
+    console.log(`Run in Port ${9000}`);
   });
 });
