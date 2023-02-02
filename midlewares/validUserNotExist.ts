856e108354000811e852e6af56f6386c281fbdbb
juanpabloct <juanptorres2003@gmail.com>
2023-02-01 22:30:21 -0500

    Middleware para validar si el correo que se esta registrando ya existe

---
+++ midlewares/validUserNotExist.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { Prisma } from "@prisma/client";
+import { client } from "../client";
+
+export const ValidUserNotExist = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email } = req.body;
+  const verifyClient = await client.user.findUnique({
+    where: { email },
+  });
+  if (verifyClient) {
+    res.status(400).json("Usuario ya existe");
+  } else {
+    next();
+  }
+};
