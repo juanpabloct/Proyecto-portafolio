c4d6b5222515b0e53e04c896f3a3651f50b27c49
juanpabloct <juanptorres2003@gmail.com>
2023-02-08 23:34:07 -0500

    Quito esa logica ya que se implemento como middleware

--- controllers/user/CreateUser.ts
+++ controllers/user/CreateUser.ts
@@ -11,13 +11,9 @@ interface InformationParams {
 export const CreateUser = async (allInformation: InformationParams) => {
   const { information, user, userAddres } = allInformation;
   const { email, password } = user;
-  information.dateOfBirth = new Date(information.dateOfBirth);
   const psswdEncrypt = EncryptPassword(password);
   return new Promise(async (resolved, rejected) => {
     try {
-      const verifyClient = await client.user.findUnique({
-        where: { email: email },
-      });
       return await resolved(
         await client.user.create({
           data: {
