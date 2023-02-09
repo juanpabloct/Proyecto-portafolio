4826640faf2b1576b63f9d68dc468bd176737e1f
juanpabloct <juanptorres2003@gmail.com>
2023-02-08 23:31:22 -0500

    Implemento para que no bloquee peticiones

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import { client } from "./client";
-
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/user", require("./router/user"));
 
 client.$connect().then(() => {
