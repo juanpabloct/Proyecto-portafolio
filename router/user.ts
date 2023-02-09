43c4848d1e978bff94e3d0c72f27c2b629936ac6
juanpabloct <juanptorres2003@gmail.com>
2023-02-08 23:35:13 -0500

    Cambio estrtuctura de try catch

--- router/user.ts
+++ router/user.ts
@@ -13,23 +13,21 @@ interface ErrorUser {
 }
 const app = Router();
 app.post("/newUser", ValidUserNotExist, async (req: Request, res: Response) => {
-  const newUser = CreateUser(req.body)
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err: ErrorUser) => {
-      res.status(err.status).json(err.error);
-    });
+  try {
+    const newUser = await CreateUser(req.body);
+    res.status(200).json(newUser);
+  } catch (err: any) {
+    res.status(err.status).json(err.error);
+  }
 });
-app.get("/login", async (req: Request, res) => {
-  const infoLogin = req.query as Prisma.UserEmailPasswordCompoundUniqueInput;
-  const loginUser = LoginUser(infoLogin)
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err: ErrorUser) => {
-      res.status(err.status).json(err.error);
-    });
+app.post("/login", async (req: Request, res) => {
+  const infoLogin = req.body as Prisma.UserEmailPasswordCompoundUniqueInput;
+  try {
+    const data = await LoginUser(infoLogin);
+    res.status(200).json(data);
+  } catch (err: any) {
+    res.status(err.status).json(err.error);
+  }
 });
 
 module.exports = app;
