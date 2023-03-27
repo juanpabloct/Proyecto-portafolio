cd1a3432e5c9a4e3890e5c699244bef4e4fa7306
juanpabloct <juanptorres2003@gmail.com>
2023-03-27 08:07:18 -0500

    add logic for add photo

--- router/user.ts
+++ router/user.ts
@@ -1,8 +1,12 @@
 import { Prisma, User } from "@prisma/client";
-import { Request, Response, Router, RequestHandler } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { UploadedFile } from "express-fileupload";
+import { awsS3 } from "../aws/connectAws";
+import { client } from "../client";
 import { CreateUser } from "../controllers/user/CreateUser";
 import { LoginUser } from "../controllers/user/loginUser";
 import { ValidUserNotExist } from "../midlewares/validUserNotExist";
+import { USER } from "../types";
 interface ReqParams {
   email: string;
   password: string;
@@ -12,22 +16,56 @@ interface ErrorUser {
   status: number;
 }
 const app = Router();
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 app.post("/newUser", ValidUserNotExist, async (req: Request, res: Response) => {
   try {
-    const newUser = await CreateUser(req.body);
-    res.status(200).json(newUser);
-  } catch (err: any) {
-    res.status(err.status).json(err.error);
-  }
-});
-app.post("/login", async (req: Request, res) => {
-  const infoLogin = req.body as Prisma.UserEmailPasswordCompoundUniqueInput;
-  try {
-    const data = await LoginUser(infoLogin);
-    res.status(200).json(data);
+    const newUser = (await CreateUser(req.body)) as User;
+    res.status(200).json({ ...newUser, password: undefined });
   } catch (err: any) {
     res.status(err.status).json(err.error);
   }
 });
 
+app.post("/login", async (req: Request, res) => {
+  const infoLogin = req.body
+    .params as Prisma.UserEmailPasswordCompoundUniqueInput;
+  try {
+    const data = await LoginUser(infoLogin);
+    res.status(200).json({ ...data, message: "Accedio Correctamente" });
+  } catch (err: any) {
+    res.status(err.status).json({ message: err.error });
+  }
+});
+app.post("/login/addPhoto", async (req: Request, res) => {
+  const image: any = req.files["image[]"];
+  const params = {
+    Bucket: "portafolios3jp",
+    Key: image.name,
+    Body: image.data,
+    ContentType: "image/jpeg",
+    accessControlPolicy: {
+      grants: [
+        {
+          permission: "FULL_CONTROL",
+        },
+      ],
+    },
+  };
+  awsS3.upload(params, function (err, data) {
+    if (err) res.status(+err.code).json({ mesagges: err.message });
+    else {
+      const url = awsS3.getSignedUrl("getObject", {
+        Bucket: data.Bucket,
+        Key: data.key,
+      });
+      client.photo.create({ data: { url, userId } });
+      res.status(200).json({
+        mesagges: `Subido en la ruta ${url}`,
+      });
+    }
+  });
+});
 module.exports = app;
