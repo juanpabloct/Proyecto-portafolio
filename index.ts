beceeb21630a2f4167819dc4cb9749e70d88c0d3
juanpabloct <juanptorres2003@gmail.com>
2023-03-27 08:02:58 -0500

    Add others routes

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
+import morgan from "morgan";
+import fileUpload from "express-fileupload";
 import { client } from "./client";
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+require("dotenv").config();
 const app = express();
+app.use(helmet());
+app.use(fileUpload());
 app.use(express.json());
 app.use(cors());
+app.use(morgan("tiny"));
 app.use("/user", require("./router/user"));
+app.use("/users", require("./router/information"));
 
 client.$connect().then(() => {
   app.listen(9000, () => {
